8b101523449a0fbc76c71c248c4d895bc319a0d1
Isaac Bilyea <isaacbilyea@gmail.com>
2024-10-17 23:14:04 -0400

    Validate HTML
    Validate HTML and fix menu

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
 (()=> {
 
-    const hamburgerMenu = document.querySelector('#hamburger-menu'),
+//VARIABLES
+
+const hamburgerMenu = document.querySelector('#hamburger-menu'),
       mainNav = document.querySelector('#main-nav'),
       body = document.querySelector('body');
 
-    const player = new Plyr('video', {
-    settings: [
-        'play-large',
-        'play',    
-        'progress', 
-        'current-time',
-        'mute',     
-        'volume',
-        'fullscreen' 
-    ]
-    });
+const player = new Plyr('video', {
+settings: [
+    'play-large',
+    'play',    
+    'progress', 
+    'current-time',
+    'mute',     
+    'volume',
+    'fullscreen' 
+]
+}); //Sets controls to remove defaults to get rid of settings
 
+
+//FUNCTIONS
+
+//Toggles classes for showing/hiding mobile menu stuff
 function toggleMenu() {
     hamburgerMenu.classList.toggle('activate');
     mainNav.classList.toggle('show');
     body.classList.toggle('menu-open');
 };
 
+
+//EVENT LISTENERS
+
 hamburgerMenu.addEventListener('click', toggleMenu);
+
 })();
