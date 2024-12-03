6185c3e7eab73046b53bce99915f42444266f02b
Isaac Bilyea <isaacbilyea@gmail.com>
2024-12-03 12:09:59 -0500

    Update custom cursor
    Update custom cursor

--- js/main.js
+++ js/main.js
@@ -99,36 +99,43 @@ hamburgerMenu.addEventListener('click', toggleMenu);
 
 
 //Intro Scroll Ball
-(()=> {
-gsap.registerPlugin(ScrollTrigger);
+(() => {
+  gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("#seperator-ball", {
-  top: "100%", 
-  ease: "linear",
-  duration: 0.1,
-  onUpdate: function() {
-    const progress = this.progress(); 
-    const welcomeSpans = document.querySelectorAll("#welcome span");
-    
+  //VARIABLES
+  const scrollBall = document.querySelector("#seperator-ball");
+  const welcomeSpans = document.querySelectorAll("#welcome span");
+
+  //FUNCTIONS
+  function changeTextColour() {
+    const progress = this.progress();
+
     if (progress === 0) {
-      //top = 0%
-      welcomeSpans[0].style.color = "#DA5A2E";
+      // top = 0%
+      welcomeSpans[0].style.color = "var(--dot-color)";
       welcomeSpans[1].style.color = "#2D2D2D";
     } else if (progress === 1) {
-      //top = 100%
+      // top = 100%
       welcomeSpans[0].style.color = "#2D2D2D";
-      welcomeSpans[1].style.color = "#DA5A2E";
+      welcomeSpans[1].style.color = "var(--dot-color)";
     }
-  },
-  scrollTrigger: {
-    trigger: "#line-seperator",
-    start: "top center",
-    end: "bottom center",
-    scrub: true,
-  },
-});
+  }
+
+  gsap.to(scrollBall, {
+    top: "100%", 
+    ease: "linear",
+    duration: 0.1,
+    onUpdate: changeTextColour,
+    scrollTrigger: {
+      trigger: "#line-seperator",
+      start: "top center",
+      end: "bottom center",
+      scrub: true,
+    },
+  });
 
 })();
+
 
 
 //----------------------------------------------------------------------------------//
@@ -231,6 +238,10 @@ gsap.to("#seperator-ball", {
 
 })();
 
+
+//----------------------------------------------------------------------------------//
+
+
 //Motion Path
 (() => {
 
@@ -276,14 +287,13 @@ gsap.to("#seperator-ball", {
 (() => {
 
 const dotCursor = document.querySelector('.dot-cursor');
-const cursorSvg = 'images/cursor.svg'; // Replace with your SVG path
+const cursorSvg = 'images/cursor.svg';
 
 document.addEventListener('mousemove', (e) => {
   dotCursor.style.top = `${e.clientY}px`;
   dotCursor.style.left = `${e.clientX}px`;
 });
 
-// Interactive elements that will trigger custom cursor
 const interactiveSelectors = [
   'a', 
   'button', 
@@ -293,22 +303,26 @@ const interactiveSelectors = [
   '[data-cursor-interact]'
 ];
 
+const cursorSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 29.73 42.01">
+  <path id="inner-cursor" d="M6,31.63l6.32-6.15s.02-.01.03-.02c.09-.08.18-.14.29-.19,0,0,0,0,0,0,.1-.04.21-.06.33-.06.01,0,.03,0,.04,0,.02,0,.05,0,.07,0,.02,0,.03,0,.04,0,.01,0,.02,0,.03,0,.02,0,.03,0,.05,0,.04,0,.07.02.1.03.03.01.07.02.1.03.04.02.09.04.13.07.03.02.06.03.09.06.04.03.08.07.11.11.02.02.05.04.06.07.04.06.08.12.11.19,0,0,.01.02.02.03h0s0,0,0,0l.02.04,6.08,13.78,2.46-1.13-6.26-13.68c-.14-.3-.12-.64.05-.92.16-.28.45-.46.78-.49l9.38-.81L6,3.32v28.32Z"/>
+  <path fill="#2d2d2d" d="M29.42,22.68L5.69.27c-.08-.08-.18-.13-.28-.18-.03-.01-.05-.02-.08-.03-.09-.03-.18-.05-.28-.06-.02,0-.04-.01-.06-.01H1C.73,0,.48.1.29.29c-.19.19-.29.44-.29.71v33c0,.55.45,1,1,1h4s.04-.01.06-.01c.09,0,.18-.02.27-.05.03-.01.06-.02.09-.03.1-.05.2-.11.28-.19h0s0-.01,0-.01l4.36-4.24,4.56,10.92c.16.37.52.61.92.61h3.83s.03,0,.05,0c.01,0,.03,0,.04,0h.07c.24,0,.46-.09.63-.23l4.06-1.86c.5-.23.72-.82.49-1.33l-6.09-13.3,10.19-.88c.39-.03.73-.29.86-.67.13-.37.03-.79-.26-1.06ZM17.04,23.41c-.32.03-.61.21-.78.49-.16.28-.18.62-.05.92l6.26,13.68-2.46,1.13-6.08-13.78-.02-.04s0,0,0,0h0s-.01-.02-.02-.03c-.03-.07-.07-.13-.11-.19-.02-.02-.04-.04-.06-.07-.04-.04-.07-.08-.11-.11-.03-.02-.06-.04-.09-.06-.04-.02-.08-.05-.13-.07-.03-.01-.07-.02-.1-.03-.03-.01-.07-.03-.1-.03-.02,0-.03,0-.05,0-.01,0-.02,0-.03,0-.01,0-.03,0-.04,0-.02,0-.05,0-.07,0-.01,0-.03,0-.04,0-.11,0-.22.02-.33.06,0,0,0,0,0,0-.11.04-.2.11-.29.19-.01,0-.02.01-.03.02l-6.32,6.15V3.32l20.42,19.29-9.38.81Z"/>
+</svg>`
+
 interactiveSelectors.forEach(selector => {
   document.addEventListener('mouseover', (e) => {
     if (e.target.closest(selector)) {
-      // Change cursor to SVG
-      dotCursor.style.backgroundImage = `url(${cursorSvg})`;
-      dotCursor.style.backgroundSize = 'contain';
-      dotCursor.style.backgroundRepeat = 'no-repeat';
-      dotCursor.style.width = '1rem';  // Adjust size as needed
-      dotCursor.style.height = '1rem'; // Adjust size as needed
+      dotCursor.innerHTML = cursorSVG;
+      dotCursor.style.backgroundColor = `transparent`;
+      dotCursor.querySelector('#inner-cursor').setAttribute('fill', 'var(--dot-color)');
+      dotCursor.style.width = '1rem';
+      dotCursor.style.height = '1rem';
     }
   });
 
   document.addEventListener('mouseout', (e) => {
     if (e.target.closest(selector)) {
-      // Revert to default dot cursor
-      dotCursor.style.backgroundImage = 'none';
+      dotCursor.innerHTML = '';
+      dotCursor.style.backgroundColor = 'var(--dot-color)';
       dotCursor.style.width = '1rem';
       dotCursor.style.height = '1rem';
     }
