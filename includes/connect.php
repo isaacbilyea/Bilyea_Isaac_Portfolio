1a48ca4f82e0a1123616a2fde791d5291b398d2e
Isaac Bilyea <isaacbilyea@gmail.com>
2025-01-18 22:14:04 -0500

    Update PHP to use PDO
    Update PHP to use PDO

--- includes/connect.php
+++ includes/connect.php
@@ -1,4 +1,13 @@
 <?php
 //Creates a connection to the database. This code is 'included' into another file, as if it is pasted into the other file.
-$connect = new mysqli('localhost','root','root','portfolio');
+//$connect = new mysqli('localhost','root','','portfolio');
+
+$dsn = "mysql:host=localhost;dbname=portfolio;charset=utf8mb4";
+ 
+try {
+$connect = new PDO($dsn, 'root', '');
+} catch (Exception $e) {
+error_log($e->getMessage());
+exit('unable to connect');
+}
 ?>
