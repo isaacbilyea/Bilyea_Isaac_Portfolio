93518a6ff1556755e3e3528f1b23719c90c3512e
Isaac Bilyea <isaacbilyea@gmail.com>
2025-02-15 16:13:01 -0500

    Upload CMS files
    Upload CMS files

---
+++ admin/login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+
+
+require_once('../includes/connect.php');
+$query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+$stmt = $connection->prepare($query);
+$stmt->bindParam(1, $_POST['username'], PDO::PARAM_STR);
+$stmt->bindParam(2, $_POST['password'], PDO::PARAM_STR);
+$stmt->execute();
+
+//if successful, one row
+//if unsuccessful, no rows
+
+if($stmt->rowCount() == 1) {
+//success stuff
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$_SESSION['username'] = $row['username'];
+
+header('Location: project_list.php');
+
+} else {
+    header('Location: login_form.php');
+}
+
+
+$stmt = null;
+?>
