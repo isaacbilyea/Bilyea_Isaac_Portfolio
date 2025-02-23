f71942adfd51f8897588a604db442491e6ab5cd9
Isaac Bilyea <isaacbilyea@gmail.com>
2025-02-23 14:27:33 -0500

    Update intro ball
    Update intro ball

--- js/modules/intro-ball.js
+++ js/modules/intro-ball.js
@@ -1,37 +1,36 @@
 export function introBall() {
+  gsap.registerPlugin(ScrollTrigger);
 
-    gsap.registerPlugin(ScrollTrigger);
+  //VARIABLES
+  const scrollBall = document.querySelector("#seperator-ball");
+  const welcomeSpans = document.querySelectorAll("#welcome span");
 
-    //VARIABLES
-    const scrollBall = document.querySelector("#seperator-ball");
-    const welcomeSpans = document.querySelectorAll("#welcome span");
-  
-    //FUNCTIONS
-    function changeTextColour() {
-      const progress = this.progress();
-  
-      //Swaps colours of welcome text based on balls progress
-      if (progress === 0) {
-        //top = 0%
-        welcomeSpans[0].style.color = "var(--dot-color)";
-        welcomeSpans[1].style.color = "#2D2D2D";
-      } else if (progress === 1) {
-        //top = 100%
-        welcomeSpans[0].style.color = "#2D2D2D";
-        welcomeSpans[1].style.color = "var(--dot-color)";
-      }
+  //FUNCTIONS
+  function changeTextColour() {
+    const progress = this.progress();
+    
+    // Use threshold values for quicker color changes
+    if (progress <= 0.05) {
+      welcomeSpans[0].style.color = "var(--dot-color)";
+      welcomeSpans[1].style.color = "#2D2D2D";
+    } else if (progress >= 0.95) {
+      welcomeSpans[0].style.color = "#2D2D2D";
+      welcomeSpans[1].style.color = "var(--dot-color)";
     }
-  
-      gsap.to(scrollBall, {
-        top: "100%", 
-        ease: "linear",
-        duration: 0.1,
-        onUpdate: changeTextColour,
-        scrollTrigger: {
-          trigger: "#line-seperator",
-          start: "top 40%",
-          end: "bottom 50%",
-          scrub: true,
-        },
-      });
+  }
+
+  // Ball animation with scrub
+  gsap.to(scrollBall, {
+      top: "100%", 
+      ease: "linear",
+      duration: 0.1,
+      onUpdate: changeTextColour,
+      scrollTrigger: {
+        trigger: "#line-seperator",
+        start: "top 40%",
+        end: "bottom 50%",
+        scrub: 1,
+        markers: false
+      },
+  });
 }
