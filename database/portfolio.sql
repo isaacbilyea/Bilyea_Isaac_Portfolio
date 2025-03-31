10450f67bbab2d65148fae0454d79520fb3bd20c
Isaac Bilyea <isaacbilyea@gmail.com>
2025-03-31 12:08:02 -0400

    Update portfolio database
    Update portfolio database

--- database/portfolio.sql
+++ database/portfolio.sql
@@ -32,7 +32,7 @@ CREATE TABLE contacts (
   name varchar(200) NOT NULL,
   email varchar(300) NOT NULL,
   message varchar(5000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table contacts
@@ -114,10 +114,10 @@ INSERT INTO contacts (id, name, email, message) VALUES
 CREATE TABLE media_files (
   id int UNSIGNED NOT NULL,
   project_id int UNSIGNED NOT NULL,
-  filename varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  type varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  alt varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  filename varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  type varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  alt varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table media_files
@@ -200,14 +200,14 @@ CREATE TABLE projects (
   cover_image varchar(500) NOT NULL,
   colour varchar(500) NOT NULL,
   overview varchar(2000) NOT NULL,
-  summary varchar(1700) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  summary varchar(1700) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   problems varchar(2500) NOT NULL,
-  research varchar(1500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  research varchar(1500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   process varchar(2500) NOT NULL,
-  final_product varchar(2300) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  reflection varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  link varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  final_product varchar(2300) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  reflection varchar(2000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  link varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table projects
@@ -231,7 +231,7 @@ CREATE TABLE users (
   id int UNSIGNED NOT NULL,
   username varchar(125) NOT NULL,
   password varchar(125) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table users
